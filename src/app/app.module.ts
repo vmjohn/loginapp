228cbceee28c5f975540940befced764c0136d07
darkcross05 <juanpa96@live.com.mx>
2018-09-11 14:51:21 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ResgisterPageComponent } from './componentes/resgister-page/resgister-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    ResgisterPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
